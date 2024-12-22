0f0a934456f28f579a2ee343655f3f3d1945f8b7
Prisha Mehta <mehtaprisha99@gmail.com>
2024-12-23 00:22:34 +0530

    optimised Bubble sort

--- Sorting_Algorithms/BubbleSort.java
+++ Sorting_Algorithms/BubbleSort.java
@@ -1,9 +1,18 @@
+// Bubble Sort is a simple sorting algorithm that repeatedly compares and swaps adjacent elements in a list until the list is sorted. After each pass, the largest unsorted element "bubbles" to its correct position.
+// Time Complexity:
+// Best Case: O(n) (when the array is already sorted)
+// Average and Worst Case: O(n²) (for unsorted or reverse order arrays)
+// Space Complexity:
+// Space Complexity: O(1) (since it sorts in place with a constant amount of extra space)
+
+
 import java.util.Scanner;
 public class BubbleSort{
     public static void bubbleSort(int arr[],int n)
     {
         for(int i=0;i<n-1;i++)
         {
+            int didSwap = 0;
             for(int j=0;j<n-i-1;j++)
             {
                 if(arr[j]>arr[j+1])
@@ -11,7 +20,11 @@ public class BubbleSort{
                     int temp = arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    didSwap = 1;
                 }
+            }
+            if(didSwap==0){
+                break;
             }
         }
     }
