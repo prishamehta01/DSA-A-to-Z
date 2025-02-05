ee40c2063e7e58c1822583d7dd3b4cf6371a5aa8
Prisha Mehta <mehtaprisha99@gmail.com>
2025-02-05 22:10:03 +0530

    Merge sort

--- Sorting_Algorithms/MergeSort.java
+++ Sorting_Algorithms/MergeSort.java
@@ -13,13 +13,7 @@ public class MergeSort{
         int i=low,j=mid+1,k=0;
         int[] temp = new int[high - low + 1];
         while(i<=mid && j<=high){
-            if(arr[i]<arr[j])
-            {
-                temp[k++] = arr[i++];
-            }
-            else{
-                temp[k++] = arr[j++];
-            }
+            temp[k++]= (arr[i]<arr[j])?arr[i++]:arr[j++];
         }
         while(i<=mid){
             temp[k++] = arr[i++];
