2fb14560aa7b02f1a8d208f5139ef5fa5debb4f9
Prisha Mehta <mehtaprisha99@gmail.com>
2025-02-08 18:30:06 +0530

    Left rotate by 1

---
+++ Arrays/Easy/LeftRotateArrayBy1.java
@@ -0,0 +1,11 @@
+
+
+import java.util.Scanner;
+public class LeftRotateArrayBy1{
+    public static void leftRotate(int[] arr,int n){
+        
+    }
+    public static void main(String[] args){
+
+    }
+}
