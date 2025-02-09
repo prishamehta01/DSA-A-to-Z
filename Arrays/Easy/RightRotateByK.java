eb408a7c873b8288c5c31625f6165de408108b16
Prisha Mehta <mehtaprisha99@gmail.com>
2025-02-09 17:37:54 +0530

    Right rotate by k

--- Arrays/Easy/RightRotateByK.java
+++ Arrays/Easy/RightRotateByK.java
@@ -26,7 +26,23 @@ public class RightRotateByK{
     */
 
    //Method - 2
-   
+    public static void rightRotate(int[] arr , int k){
+        int n = arr.length;
+        k = k % n; //for k>n
+        Reverse(arr,0,n-1);
+        Reverse(arr,0,k-1);
+        Reverse(arr,k,n-1);
+    }
+    private static void Reverse(int[] arr, int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start]= arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
     public static void main(String[] args){
         int n;
         Scanner sc = new Scanner(System.in);
