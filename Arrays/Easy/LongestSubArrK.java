7900ff74074f9a6b904718c1fc6defe5a96b500f
Prisha Mehta <mehtaprisha99@gmail.com>
2025-02-17 19:15:39 +0530

    Longest Substring with sum K - brute force

---
+++ Arrays/Easy/LongestSubArrK.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+public class LongestSubArrK{
+    /*
+    
+    //Brute Force Method- Finding All possible SubArrays - time Complexity - O(n^3)
+    public static int getLongestSub(int[] arr, int K){
+        int n = arr.length;
+        int len = 0 ;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int sum = 0;
+                for(int k=i;k<=j;k++){
+                    sum+=arr[k];
+                }
+                if(sum==K){
+                    len = Math.max(len,j-i+1);
+                }
+            }
+        }
+        return len;
+    }
+    
+    */
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of array elements: ");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        System.out.println("Enter the array elements: ");
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.println("Enter the sum K: ");
+        int K = sc.nextInt();
+        int LongestSubArrLen = getLongestSub(a,K);
+        System.out.println("Length of largest Subarray with sum K is :"+LongestSubArrLen);
+    }
+}
