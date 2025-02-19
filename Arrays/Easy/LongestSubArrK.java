8fcaf7ae979c7d131d4df2cd2a50e65d673b957e
Prisha Mehta <mehtaprisha99@gmail.com>
2025-02-19 23:20:39 +0530

    Longest Substring with sum K - Optimal Approach

--- Arrays/Easy/LongestSubArrK.java
+++ Arrays/Easy/LongestSubArrK.java
@@ -45,7 +45,30 @@ public class LongestSubArrK{
 */
     //Optimal Approach - Two Pointers - Time Complexity:O(2*N) and Space Complexity:O(1)
     public static int getLongestSub(int[] arr, int k) {
-        
+        int n = a.length; // size of the array.
+
+        int left = 0, right = 0; // 2 pointers
+        long sum = a[0];
+        int maxLen = 0;
+        while (right < n) {
+            // if sum > k, reduce the subarray from left
+            // until sum becomes less or equal to k:
+            while (left <= right && sum > k) {
+                sum -= a[left];
+                left++;
+            }
+
+            // if sum = k, update the maxLen i.e. answer:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Move forward thw right pointer:
+            right++;
+            if (right < n) sum += a[right];
+        }
+
+        return maxLen;
     }
     
     public static void main(String[] args){
