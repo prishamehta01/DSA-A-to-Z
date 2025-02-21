30c20275ff78506d1e031a2a9e892f9df5ce17e8
Prisha Mehta <mehtaprisha99@gmail.com>
2025-02-21 23:46:09 +0530

    Maximum Consecutive Ones

--- Arrays/Easy/MaxConsecutiveOnes.java
+++ Arrays/Easy/MaxConsecutiveOnes.java
@@ -1,6 +1,6 @@
 //Approach -  We maintain a variable cnt that keeps a track of the number of consecutive 1’s while traversing the array. The other variable max_count maintains the maximum number of 1’s.
 //Time Complexity: O(N)[single pass] , Space Complexity: O(1)[No extra memory used]
-import java.util.*;
+
 public class MaxConsecutiveOnes{
     public static int findMaxConsecutiveOnes(int[] nums){
         int cnt=0,max_cnt=0;
