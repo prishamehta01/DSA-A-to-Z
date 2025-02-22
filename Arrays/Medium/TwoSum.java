2bbc8f8e2fa91d7181c9bce61b2fbf8e4746d406
Prisha Mehta <mehtaprisha99@gmail.com>
2025-02-22 09:07:58 +0530

    Two sum optimal Approach - 2 pointers

--- Arrays/Medium/TwoSum.java
+++ Arrays/Medium/TwoSum.java
@@ -2,7 +2,7 @@
 Two Sum Problem - Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 */
 
-import java.util.HashMap;
+import java.util.*;
 
 class TwoSum{
     
@@ -17,7 +17,10 @@ class TwoSum{
         }
         return new int[]{};
     }
-*/
+
+
+    //Better Approach - Hashing -Time Complexity:O(N) - Space complexity:O(N)
+    //We have optimized this problem enough. But if in the interview, we are not allowed to use the map data structure, then we should move on to the next approach below i.e. two pointer approach. This approach will have the same time complexity as the better approach.
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
         res[0]=res[1]=-1;
@@ -35,10 +38,52 @@ class TwoSum{
         return res;
     }
 
+    //Optimal if just returning where target is found or not
+    public static String twoSum(int n, int []arr, int target) {
+        Arrays.sort(arr);
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                return "YES";
+            } else if (sum < target) left++;
+            else right--;
+        }
+        return "NO";
+    }
+
+*/
+    //Optimal Approach - Two Pointer - Time Complexity : O(N) + O(N*logN)
+    //In this approach, we will first sort the array and will try to choose the numbers in a greedy way.
+    public int[] twoSum(int[] nums, int target){
+        int[][] pairs = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
+            pairs[i][0] = nums[i];  // value
+            pairs[i][1] = i;        // original index
+        }
+        
+        // Sort based on values
+        Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
+        
+        // Two pointer approach
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int sum = pairs[left][0] + pairs[right][0];
+            if (sum == target) {
+                return new int[]{pairs[left][1], pairs[right][1]};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{};
+}
+    
     public static void main(String[] args){
         TwoSum obj = new TwoSum();
-        int[] arr = {2,7,11,15};
-        int target = 9;
+        int[] arr = {2,7,11,2};
+        int target = 4;
         int[] res =  obj.twoSum(arr,target);
         for(int a:res){
             System.out.print(a+" ");
