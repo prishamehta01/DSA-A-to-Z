7ca890f7d21ca8dd6f2a3b3393e292db1a47ea10
Prisha Mehta <mehtaprisha99@gmail.com>
2025-02-23 16:15:21 +0530

    Sort colors- sort arrays of 0s,1s and 2s- Optimal Approach

--- Arrays/Medium/SortColors.java
+++ Arrays/Medium/SortColors.java
@@ -35,8 +35,26 @@ public class SortColors {
 */
 //Optimal Approach - This problem is a variation of the popular Dutch National flag algorithm. 
 //Time Complexity : O(N)
-    public static void sortColors(int[] nums){
-        
+    public static void sortColors(int[] nums) {
+        int low=0,mid=0,high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp = nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high]=temp;
+                high--;
+            }
+        }
     }
     public static void main(String[] args) {
         int[] arr = {2,0,2,1,1,0};
