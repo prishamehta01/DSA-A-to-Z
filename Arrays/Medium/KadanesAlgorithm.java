fd4c05e19a8b76f630c03444fdf27b395fb04d15
Prisha Mehta <mehtaprisha99@gmail.com>
2025-02-25 22:09:13 +0530

    Kadanes Algo-Optimal Approach-Sub Array with max sum

--- Arrays/Medium/KadanesAlgorithm.java
+++ Arrays/Medium/KadanesAlgorithm.java
@@ -25,9 +25,10 @@ public class KadanesAlgorithm {
     }
 */
 
-    //Optimal Approach : The intuition of the algorithm is not to consider the subarray as a part of the answer if its sum is less than 0. A subarray with a sum less than 0 will always reduce our answer and so this type of subarray cannot be a part of the subarray with maximum sum.
-    //Time Complexity: O(N)
-    public static int maxSubArray(int[] nums) {
+//Optimal Approach : The intuition of the algorithm is not to consider the subarray as a part of the answer if its sum is less than 0. A subarray with a sum less than 0 will always reduce our answer and so this type of subarray cannot be a part of the subarray with maximum sum.
+//Time Complexity: O(N)
+    
+    /*public static int maxSubArray(int[] nums) {
         int sum=0;
         int maxi=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
@@ -41,7 +42,36 @@ public class KadanesAlgorithm {
         }
         return maxi;
     }
+    */
     
+    //Follow up Question - if we need to print the sub array too...
+    public static int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxi = Integer.MIN_VALUE;
+        int start = 0;
+        int startAns = -1, endAns = -1;
+        for(int i=0;i<nums.length;i++){
+            if(sum==0)
+                start = i;
+            sum+=nums[i];
+            if(sum>maxi){
+                maxi=sum;
+                startAns = start;
+                endAns = i;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+
+        System.out.print("The SubArray with Largest sum is :[");
+        for(int i=startAns;i<=endAns;i++){
+            System.out.print(nums[i]+" ");
+        }
+        System.out.print("]\n");
+        return maxi;
+    }
+
     public static void main(String[] args) {
         int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
         int maxSum = maxSubArray(arr);
