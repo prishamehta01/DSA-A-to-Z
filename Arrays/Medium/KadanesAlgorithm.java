ac6c705b7389effa599a530bd65239c2069df4da
Prisha Mehta <mehtaprisha99@gmail.com>
2025-02-25 21:57:17 +0530

    Kadanes Algo-Optimal Approach-Sub Array with max sum

--- Arrays/Medium/KadanesAlgorithm.java
+++ Arrays/Medium/KadanesAlgorithm.java
@@ -24,6 +24,23 @@ public class KadanesAlgorithm {
         return maxi;
     }
 */
+
+    //Optimal Approach : The intuition of the algorithm is not to consider the subarray as a part of the answer if its sum is less than 0. A subarray with a sum less than 0 will always reduce our answer and so this type of subarray cannot be a part of the subarray with maximum sum.
+    //Time Complexity: O(N)
+    public static int maxSubArray(int[] nums) {
+        int sum=0;
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>maxi){
+                maxi=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxi;
+    }
     
     public static void main(String[] args) {
         int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
