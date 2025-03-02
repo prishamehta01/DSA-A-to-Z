f76bfe7c424e95fdc26b17963f0189cab7837a49
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-02 21:28:59 +0530

    Buy and sell with max profit

--- Arrays/Medium/BuyAndSell.java
+++ Arrays/Medium/BuyAndSell.java
@@ -22,8 +22,14 @@ public class BuyAndSell {
 */
 
     //Optimal Approach
-    public static int maxProfit(int[] prices){
-        
+    public static int maxProfit(int[] prices) {
+        int maxPro = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++){
+            min = Math.min(min, prices[i]);
+            maxPro = Math.max(maxPro, prices[i]-min);
+        }
+        return maxPro;
     }
     public static void main(String[] args) {
         int arr[] = {7,1,5,3,6,4};
