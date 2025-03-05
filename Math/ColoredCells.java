92e3df6f93d65ceb3b0be90fe3870bac45f8e541
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-05 23:28:09 +0530

    Colored Cell after n minutes

--- Math/ColoredCells.java
+++ Math/ColoredCells.java
@@ -1,3 +1,14 @@
+/*
+There exists an infinitely large two-dimensional grid of uncolored unit cells. You are given a positive integer n, indicating that you must do the following routine for n minutes:
+At the first minute, color any arbitrary unit cell blue.
+Every minute thereafter, color blue every uncolored cell that touches a blue cell.
+
+Example:
+    Input: n = 2
+    Output: 5
+    Explanation: After 2 minutes, there are 4 colored cells on the boundary and 1 in the center, so we return 5.  */
+
+
 import java.util.Scanner;
 public class ColorCells{
 /* 
