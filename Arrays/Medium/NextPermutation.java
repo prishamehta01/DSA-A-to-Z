026f4d7816ef13c0cdcc984fc30af9f977e88fa8
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-07 17:40:51 +0530

    Next permutation optimal approach

---
+++ Arrays/Medium/NextPermutation.java
@@ -0,0 +1,42 @@
+public class NextPermutation {
+    public void rev(int[] nums,int left,int right){
+        while(left<right){
+            int temp = nums[left];
+            nums[left]= nums[right];
+            nums[right]= temp;
+            left++;
+            right--;
+        }
+    }
+    //Optimal Approach - Time Complexity :O(n)
+    public void nextPermutation(int[] nums) {
+        int index = -1;
+        for(int i=nums.length-2;i>=0;i--){  //finding break point
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1){  //if no break point array is sorted, so reverse the array
+            rev(nums,0,nums.length-1);
+            return;
+        }
+        for(int i=nums.length-1;i>index;i--){  //finding a number greater than nums[index] but closer
+            if(nums[i]>nums[index]){
+                int temp = nums[i];
+                nums[i]=nums[index];
+                nums[index]=temp;
+                break;
+            }
+        }
+        rev(nums,index+1,nums.length-1); //reverse the remaining elements to make it just the next permutation
+    }
+    public static void main(String[] args) {
+        NextPermutation np = new NextPermutation();
+        int[] arr = {1,2,3};
+        np.nextPermutation(arr);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+}
