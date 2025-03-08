da408a2e0a1eae506780e1cf844cfd470821707d
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-08 22:02:49 +0530

    Minimum Recolors

---
+++ String/Easy/MinRecolors.java
@@ -0,0 +1,28 @@
+public class MinRecolors{
+    public static int minimumRecolors(String blocks, int k) {
+        int whites = 0;
+        int minWhites = k;
+        for(int i=0;i<k;i++){
+            if(blocks.charAt(i)=='W'){
+                whites++;
+            }
+        }
+        minWhites = whites;
+        for(int i=k;i<blocks.length();i++){
+            if (blocks.charAt(i - k) == 'W') {
+                whites--;
+            }
+            if (blocks.charAt(i) == 'W') {
+                whites++;
+            }
+            minWhites = Math.min(minWhites,whites);
+        }
+        return minWhites;
+    }
+    public static void main(String[] args){
+        String s = "WBBWWBBWBW";
+        int k = 7;
+        int minRecolors = minimumRecolors(s,k);
+        System.out.println("The minimum number of recolors would be: "+minRecolors);
+    }
+}
