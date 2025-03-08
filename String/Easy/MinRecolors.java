6c3b935db2f9addb6a71965c50eb2e9c175e2dc1
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-08 22:21:51 +0530

    Minimum Recolors

--- String/Easy/MinRecolors.java
+++ String/Easy/MinRecolors.java
@@ -1,4 +1,22 @@
 public class MinRecolors{
+
+//Brute Force - Time Complexity:O(n^2)
+/*  public static int minimumRecolors(String blocks, int k) {
+        int minWhites = k;
+        for(int i = 0; i <= blocks.length() - k; i++) { // Fix off-by-one error
+            int whites = 0;
+            for(int j = 0; j < k; j++) {
+                if(blocks.charAt(i + j) == 'W') {
+                    whites++;
+                }
+            }
+            minWhites = Math.min(minWhites, whites);
+        }
+        return minWhites;
+    }
+*/
+
+    //Better Approach - Time Complexity:O(n)
     public static int minimumRecolors(String blocks, int k) {
         int whites = 0;
         int minWhites = k;
