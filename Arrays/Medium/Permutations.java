70bd876872b6eb42b7772fae12c98ac61faba5aa
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-09 16:33:31 +0530

    Permutation

---
+++ Arrays/Medium/Permutations.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generatePermutations(nums, 0, result);
+        return result;
+    }
+
+    private void generatePermutations(int[] nums, int start, List<List<Integer>> result) {
+        if (start == nums.length) {
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            result.add(permutation);
+            return;
+        }
+        
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i); // Swap current element with start
+            generatePermutations(nums, start + 1, result); // Recurse for next index
+            swap(nums, start, i); // Backtrack (undo swap)
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void main(String[] args) {
+        Permutations obj = new Permutations();
+        int[] nums = {1, 2, 3};
+        System.out.println(obj.permute(nums));
+    }
+}
