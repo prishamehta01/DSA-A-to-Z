b2ff5a61625d0dd6f420a9c21ade430c6b929e49
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-22 12:21:06 +0530

    Power of 2

--- Math/PowerOf2.java
+++ Math/PowerOf2.java
@@ -2,6 +2,27 @@ import java.util.Scanner;
 
 public class PowerOf2{
 
+    //Optimal Approach -- constraint:No bitwise operators allowed -- O(1)
+    /*
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        double res = Math.log(n)/Math.log(2);
+        return (res == (int)res);
+    }
+     */
+
+    //Optimal Approach 2 -- O(logn)
+    /*
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        while(n%2==0){
+            n/=2;
+        }
+        return n==1;
+    }
+     */
+
+    //Optimal Approach -- O(1)
     public static boolean isPowerOfTwo(int n) {
         if(n<=0) return false;
         if((n&(n-1))==0){
