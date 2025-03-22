482abda7474a5e047c144e2ee1006745d3635614
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-22 12:10:43 +0530

    Power of 2

--- Math/PowerOf2.java
+++ Math/PowerOf2.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
 public class PowerOf2{
+
+    public static boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        if((n&(n-1))==0){
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if(n>0 && (n&(n-1))== 0){
-            System.out.println("YES");
-        }
-        else{
-            System.out.println("NO");
-        }
+        boolean res = isPowerOfTwo(n);
+        System.out.println(res);
         sc.close();
     }
 }
