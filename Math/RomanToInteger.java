6586d9ae5dfc2f8590051e872c422f9100037ea4
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-25 22:39:23 +0530

    Roman to Integer

--- Math/RomanToInteger.java
+++ Math/RomanToInteger.java
@@ -1,4 +1,30 @@
 public class RomanToInteger {
+
+    /*Brute force 
+     public int romanToInt(String s) {
+        HashMap<Character,Integer> hp = new HashMap<>();
+        hp.put('I',1);
+        hp.put('V',5);
+        hp.put('X',10);
+        hp.put('L',50);
+        hp.put('C',100);
+        hp.put('D',500);
+        hp.put('M',1000);
+        int res = 0;
+        for(int i=0;i<s.length();i++){
+            int val = hp.get(s.charAt(i));
+            if(i<s.length()-1 && val < hp.get(s.charAt(i+1))){
+                res-=val;
+            }
+            else{
+                res+=val;
+            }
+        }
+        return res;
+    }
+     */
+
+     
     public static int romanToInt(String s) {
         int[] values = {1, 5, 10, 50, 100, 500, 1000};
         char[] symbols = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
