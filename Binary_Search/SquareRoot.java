19f49ddfb1d54d026468f46330c23876acc7cd6b
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-29 12:21:31 +0530

    Square Root

---
+++ Binary_Search/SquareRoot.java
@@ -0,0 +1,28 @@
+public class SquareRoot {
+    static int sqrt(int n){
+        int beg = 0;
+        int end = n-1;
+        int res = -1;
+        while(beg<end){
+            int mid = (beg+end)/2;
+            if((mid*mid) == n){
+                res = mid;
+                return mid;
+            }
+            else{
+                if((mid*mid) > n){
+                    end = mid - 1;
+                }
+                else{
+                    beg = mid+1;
+                }
+            }
+        }
+        return res;
+        
+    }
+    public static void main(String[] args) {
+        int res = sqrt(36);
+        System.out.println(res);
+    }
+}
