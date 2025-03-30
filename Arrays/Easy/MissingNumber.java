0924788b885a2f51d964b5b31447704d2acdf33b
Prisha Mehta <mehtaprisha99@gmail.com>
2025-03-30 15:53:15 +0530

    Missing number

--- Arrays/Easy/MissingNumber.java
+++ Arrays/Easy/MissingNumber.java
@@ -39,6 +39,27 @@ public class MissingNumber{
     }
     */
 
+    //Method 3 --- works only if sorted 
+    /*public static int missingNumber(int[] nums){
+        int n = nums.length;
+        int s = 0;
+        int e = n-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(nums[mid] != mid+1){
+                if(mid==0 || nums[mid-1]==mid){
+                    return mid+1;
+                }
+                e = mid-1;
+            }
+            else{
+                s = mid + 1;
+            }
+        }
+        return n+1;
+    }
+    */
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of array elements: ");
