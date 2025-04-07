857dc644fe4a0c4fc4a798205ee84cfac72c5f83
Prisha Mehta <mehtaprisha99@gmail.com>
2025-04-07 22:00:58 +0530

    Rotate Array

--- Arrays/Medium/RotateArray.java
+++ Arrays/Medium/RotateArray.java
@@ -1,5 +1,7 @@
 public class RotateArray {
-    public static void rotate(int[][] matrix) {
+
+    //Time Complexity:O(n*n), Space Complexity:O(n*n)
+    /*public static void rotate(int[][] matrix) {
         int n = matrix.length;
         int[][] rotated = new int[n][n];
         for(int i = 0;i<n;i++){
@@ -10,6 +12,26 @@ public class RotateArray {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 matrix[i][j] = rotated[i][j];
+            }
+        }
+    }*/
+
+    //Optimal Approach -- Find Transpose and reverse the row -- Space Complexity:O(1)
+    public static void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
             }
         }
     }
