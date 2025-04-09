b2fc30cca214833b7eb1b2cdd32795256ef88fa0
Prisha Mehta <mehtaprisha99@gmail.com>
2025-04-09 15:58:35 +0530

    Minimum Number of Operations to Make Elements in Array Distinct

--- Arrays/Easy/MinOpr.java
+++ Arrays/Easy/MinOpr.java
@@ -23,3 +23,37 @@ public class MinOpr {
         System.out.println(res);
     }
 }
+
+
+/*
+Variety 2:
+You are given an integer array nums and an integer k.
+
+An integer h is called valid if all values in the array that are strictly greater than h are identical.
+
+For example, if nums = [10, 8, 10, 8], a valid integer is h = 9 because all nums[i] > 9 are equal to 10, but 5 is not a valid integer.
+
+You are allowed to perform the following operation on nums:
+
+Select an integer h that is valid for the current values in nums.
+For each index i where nums[i] > h, set nums[i] to h.
+Return the minimum number of operations required to make every element in nums equal to k. If it is impossible to make all elements equal to k, return -1.
+
+
+
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Set<Integer> s = new HashSet<>();
+        for(int n:nums){
+            if(n<k){
+                return -1;
+            }
+            else if(n>k){
+                s.add(n);
+            }
+        }
+        return s.size();
+    }
+}
+ */
