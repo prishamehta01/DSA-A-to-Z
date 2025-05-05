9bb2b09148d8e00b23c13a046e04b0a5d5f54783
Prisha Mehta <mehtaprisha99@gmail.com>
2025-05-05 23:33:40 +0530

    Trapping rain water

--- Arrays/Hard/TrappingRainWater.java
+++ Arrays/Hard/TrappingRainWater.java
@@ -6,6 +6,32 @@ Output: 9
 
 public class TrappingRainWater{
 
+
+/*
+Initial Approach - O(n),O(n)
+    public int trap(int[] height) {
+        int res = 0;
+        int n = height.length;
+        int[] maxLeft = new int[n];
+        int[] maxRight = new int[n];
+        maxLeft[0] = height[0];
+        for(int i=1;i<n;i++){
+            maxLeft[i] = Math.max(maxLeft[i-1],height[i]);
+        }
+        maxRight[n-1] = height[n-1];
+        for(int i=n-2;i>=0;i--){
+            maxRight[i] = Math.max(maxRight[i+1],height[i]);
+        }
+
+        for(int i=0;i<n;i++){
+            if(Math.min(maxLeft[i],maxRight[i])-height[i]>0){
+                res+=Math.min(maxLeft[i],maxRight[i])-height[i];
+            }
+        }
+        return res;
+    }
+*/
+
     //Optimal Approach - Two Pointers
      public static int trap(int[] height) {
         int n = height.length;
